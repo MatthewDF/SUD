24314c80a0ba773305376b6070635309bde1b223
MatthewDF <MattDFrazier@gmail.com>
2020-09-25 11:25:27 -0500

    Re-work of GUI window
    GUI Window handles text input and display. Still needs to handle output, text-wrapping, and resizing.

--- SUDWindow.py
+++ SUDWindow.py
@@ -3,13 +3,29 @@ import tkinter as tk
 
 window = tk.Tk()
 
-# Create a window that will receive text from the rest of the program and display it for the user. Get width of window
-# and if the text is too wide, wrap it.
+# Creates a window to display text to the user and allow the user to input commands
 
-# Window drawing
+
+# Take current text in user input field and submit
+def input_handler(event=None):
+    user_input = entry_field.get()
+    entry_field.delete(0, tk.END)
+
+    window_txt_display.insert(tk.END, "\n" + user_input)
+
+    # Send user input to command module to see if their input matches a command
+
+    # A return value will be sent by command module if command is not a match. If that's so, print error msg
+
+
+# Write output to screen. If text length is too long, wrap it
+def output():
+    pass
+
+
 frame_display = tk.Frame(master=window)
 frame_display.grid(column=0, row=1)
-window_txt_display = tk.Text(master=frame_display, height=30, width=60, bg="black")
+window_txt_display = tk.Text(master=frame_display, height=30, width=60, bg="black", fg="white")
 window_txt_display.pack(fill=tk.BOTH)
 
 frame_text = tk.Frame(master=window)
@@ -19,10 +35,9 @@ entry_field.pack()
 
 frame_button = tk.Frame(master=window)
 frame_button.grid(column=0, row=2, sticky="e")
-submit = tk.Button(master=frame_button, text="Submit", height=2, width=8)
+submit = tk.Button(master=frame_button, text="Submit", command=input_handler, height=2, width=8)
 submit.pack(side=tk.RIGHT, fill=tk.BOTH)
 
+entry_field.bind('<Return>', input_handler)
+
 window.mainloop()
-
-# Draw submit button
-
